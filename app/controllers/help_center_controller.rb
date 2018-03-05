ba92221c26ad3264581f2a08717beac11d5199d8
Sandeep <sandeep.chourey@3scsolution.com>
2018-03-05 13:00:54 +0530

    modi file

--- app/controllers/help_center_controller.rb
+++ app/controllers/help_center_controller.rb
@@ -3,7 +3,7 @@ class HelpCenterController < ApplicationController
   	@networks = Truck.pluck(:truck_from).flatten.compact.uniq
     @lat_lng = []
     # @networks = @networks.present? ? @networks : ["Pune","Mumbai"]
-    @networks = ["Pune","Mumbai"]
+    @networks = @networks.present? ? @networks : ["Pune","Mumbai"]
   	@networks.each do |t|
   		source_lat_long = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{t}&key=AIzaSyAlvMtnmGT7_fFtVgvKtDMG5tYuIX2Rdig", :verify => false)
       @lat_lng << source_lat_long["results"][0]["geometry"]["location"]
