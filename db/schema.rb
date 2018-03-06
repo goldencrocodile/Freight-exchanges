b11362185e74586a604f9ad809417c4001313cb3
Sandeep <sandeep.chourey@3scsolution.com>
2018-03-06 10:45:09 +0530

    ADD removing file

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180213061741) do
+ActiveRecord::Schema.define(version: 20180305133010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_admin_comments", force: true do |t|
+    t.string   "namespace"
+    t.text     "body"
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
+    t.integer  "author_id"
+    t.string   "author_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
+  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
+
+  create_table "admin_users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
+  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "cities", force: true do |t|
     t.string   "city_name"
@@ -46,12 +79,26 @@ ActiveRecord::Schema.define(version: 20180213061741) do
     t.string   "load_type"
     t.string   "source_pin_code"
     t.string   "destination_pin_code"
+    t.boolean  "load_enabled",         default: true
+    t.boolean  "booked",               default: false
+    t.integer  "user_id"
+  end
+
+  create_table "orders", force: true do |t|
+    t.string   "product_id"
+    t.string   "price"
+    t.string   "status"
+    t.string   "payment_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "roles", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "role_enabled", default: true
   end
 
   create_table "trucks", force: true do |t|
@@ -65,21 +112,23 @@ ActiveRecord::Schema.define(version: 20180213061741) do
     t.string   "truck_to"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "truck_enabled",    default: true
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                   default: "", null: false
-    t.string   "encrypted_password",      default: "", null: false
+    t.string   "email",                         default: "", null: false
+    t.string   "encrypted_password",            default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",           default: 0,  null: false
+    t.integer  "sign_in_count",                 default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                           null: false
-    t.datetime "updated_at",                           null: false
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
     t.string   "u_type"
     t.string   "first_name"
     t.string   "last_name"
@@ -99,6 +148,10 @@ ActiveRecord::Schema.define(version: 20180213061741) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "aadhaar_no_image_file_name"
+    t.string   "aadhaar_no_image_content_type"
+    t.integer  "aadhaar_no_image_file_size"
+    t.datetime "aadhaar_no_image_updated_at"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
