bc23943015de01ca5654a4e9b0b071fdb25e7676
Sandeep <sandeep.chourey@3scsolution.com>
2018-03-06 17:41:38 +0530

    modification from css data picker

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ FreightExchange::Application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-  config.assets.digest = false
+  # config.assets.digest = false
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
